dff4352cfb4ffbdc6e3a968330db5ed10f8f6355
Mario Chavez <mariochavez1292@gmail.com>
2018-04-12 08:43:25 -0500

    pepe

--- app/src/main/java/com/example/alumno/examenprimerparcialmariochavez/MainActivity.kt
+++ app/src/main/java/com/example/alumno/examenprimerparcialmariochavez/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.alumno.examenprimerparcialmariochavez
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun valida(view: View){
+        var a:EditText= findViewById<EditText>(R.id.c)
+        var b:EditText= findViewById<EditText>(R.id.cc)
+        Log.d("aaaaaaaaaaaaaaaaaaaaaa","aaaaaaaaaaaaaaaaaaaaaaaaa")
+        if(a.text.toString() == "lalo" && b.text.toString() == "lalo"){
+            startActivity(Intent(this, MenusPerroActivity::class.java))
+        }else{
+            Toast.makeText(this,"aaaaaaaaaaaaaaaaaaa",Toast.LENGTH_SHORT)
+        }
     }
 }
---
+++ app/src/main/java/com/example/alumno/examenprimerparcialmariochavez/MenusPerroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.alumno.examenprimerparcialmariochavez
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MenusPerroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menus_perro)
+    }
+}
